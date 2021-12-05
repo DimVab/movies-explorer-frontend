704ccbd992068babac2e23b3b09a24b8d2c68e90
Dmitry Vabischevich <Dmitryvabischevich@gmail.com>
2021-12-05 18:22:05 +0300

    feat: добавить компонент portfolio

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,3 +1,5 @@
+import avatar from '../../images/unknown.png';
+
 function AboutMe () {
 
   return(
@@ -6,12 +8,12 @@ function AboutMe () {
         <div className="about-me__info">
           <article className="about-me__article">
               <h3 className="about-me__name">Дмитрий</h3>
-              <p className="about-me__info">
+              <p className="about-me__briefly">
               Фронтенд-разработчик, 26 лет
               </p>
               <p className="about-me__description">
               Я родился в Москве, живу в Московской области, Нахабино. Закончил юрфак одного из Московских вузов, работаю на госслужбе.
-              Люблю осваивать что-то новое. Недавно начал кодить. Сейчас заканчиваю курс по веб-разработке. Надеюсь в будущем трудоустроиться в качестве разработчика (если не переведусь в "Н";).
+              Люблю осваивать что-то новое. Недавно начал кодить. Сейчас заканчиваю курс по веб-разработке. В близжайшем будущем постараюсь трудоустроиться в качестве разработчика (если не переведусь в "Н";).
               </p>
           </article>
           <ul className="about-me__links">
@@ -19,7 +21,7 @@ function AboutMe () {
             <li className="about-me__links-element"><a href="https://github.com/DimVab" target="_blank" rel="noreferrer" className="about-me__link">Github</a></li>
           </ul>
         </div>
-        <img src="#" className="about-me__photo" alt="Фотография студента" />
+        <img src={avatar} className="about-me__photo" alt="Фотография студента" />
       </div>
     </div>
   );
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,9 +1,31 @@
 function Portfolio () {
 
   return(
-    <section className="portfolio">
-      456
-    </section>
+    <div className="portfolio">
+      <h3 className="portfolio__heading">Портфолио</h3>
+      <ul className="portfolio__sites">
+        <li className="portfolio__site">
+          <a href="https://github.com/DimVab/how-to-learn" target="_blank" rel="noreferrer" className="portfolio__link">
+            <p className="portfolio__link-name">Статичный сайт</p>
+            <p className="portfolio__link-icon">↗</p>
+          </a>
+          <div className="portfolio__line"></div>
+        </li>
+        <li className="portfolio__site">
+          <a href="https://github.com/DimVab/russian-travel" target="_blank" rel="noreferrer" className="portfolio__link">
+            <p className="portfolio__link-name">Адаптивный сайт</p>
+            <p className="portfolio__link-icon">↗</p>
+          </a>
+          <div className="portfolio__line"></div>
+        </li>
+        <li className="portfolio__site">
+          <a href="https://github.com/DimVab/react-mesto-api-full" target="_blank" rel="noreferrer" className="portfolio__link">
+            <p className="portfolio__link-name">Одностраничное приложение</p>
+            <p className="portfolio__link-icon">↗</p>
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
