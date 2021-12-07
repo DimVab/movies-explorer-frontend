61a0d055a06e0ebdb270b662d3c73cde2b0d162f
Dmitry Vabischevich <Dmitryvabischevich@gmail.com>
2021-12-07 15:24:19 +0300

    feat: добавить переменные состояния в app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,46 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Sign from '../Sign/Sign';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
 import Menu from '../Menu/Menu';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
 function App() {
 
+  const [loggedIn, sign] = React.useState(true);
+  const [isOpened, openMenu] = React.useState(false);
+  const [isLoading, loadCards] = React.useState(false);
+  const [userInfo, setUserInfo] = React.useState({ name: 'Дмитрий', email: 'pochta@mail.ru'});
+
   return (
     <><Switch>
+      <Route exact path="/sign-up">
+        <Sign />
+      </Route>
+      <Route exact path="/sign-in">
+        <Sign />
+      </Route>
       <Route exact path="/">
-        <Main loggedIn={true} />
+        <Main loggedIn={loggedIn} />
       </Route>
       <Route exact path="/movies">
-        <Movies loggedIn={true} isLoading={false} />
+        <Movies loggedIn={loggedIn} isLoading={isLoading} />
       </Route>
       <Route exact path="/saved-movies">
-        <SavedMovies loggedIn={true} isLoading={false} />
+        <SavedMovies loggedIn={true} isLoading={isLoading} />
+      </Route>
+      <Route exact path="/profile">
+        <Profile loggedIn={loggedIn} userInfo={userInfo} />
       </Route>
       <Route path="*">
         <NotFoundPage />
       </Route>
     </Switch>
 
-    <Menu isOpened={false} currentPage="menu" /></>
+    <Menu isOpened={isOpened} currentPage="menu" /></>
   );
 }
 
