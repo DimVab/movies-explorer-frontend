637c671031d2a237b708d32d0fa32339c5cc4935
Dmitry Vabischevich <Dmitryvabischevich@gmail.com>
2021-12-20 04:50:15 +0300

    feat: сделать валидацию редактирования профиля!

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,6 +2,8 @@ import { useState, useEffect, useContext } from 'react';
 import Header from '../Header/Header';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import validators from '../Validators/Validators';
+import errorMessages from '../../utils/errorMessages';
 
 function Profile ({ onEdit, setEdit, reqError, reqErrorText, openMenu, handleEditProfile, handleSignOut }) {
 
@@ -16,6 +18,91 @@ function Profile ({ onEdit, setEdit, reqError, reqErrorText, openMenu, handleEdi
     email: userInfo.email
   });
 
+  const [errors, setErrors] = useState({
+    userName: {
+      required: false,
+      minLength: false,
+      isCorrect: false,
+    },
+    userEmail: {
+      required: false,
+      isEmail: false,
+    }
+  });
+
+  const [isSubmitDisabled, setSubmitDisabled] = useState(true);
+
+  const [errorTexts, setErrorTexts] = useState({
+    userNameErrorText: '',
+    userEmailErrorText: ''
+  });
+
+  useEffect(() => {
+    const isNameInputValid = !Object.values(errors.userName).some(Boolean);
+    const isEmailInputValid = !Object.values(errors.userEmail).some(Boolean);
+    const areInputsNew = profileData.name !== userInfo.name || profileData.email !== userInfo.email;
+
+    if (isNameInputValid && isEmailInputValid && areInputsNew) {
+      setSubmitDisabled(false);
+    } else {
+      setSubmitDisabled(true);
+    }
+  }, [errors]);
+
+    // реагирует на изменение имени
+  useEffect(() => {
+    const { name } = profileData;
+
+    const userNameValidationResult = Object.keys(validators.userName)
+      .map((errorKey) => {
+        const errorResult = validators.userName[errorKey](name);
+        return { [errorKey]: errorResult};
+      })
+      .reduce((acc, el) => ({ ...acc, ...el }), {});
+
+      setErrors({
+        ...errors,
+        userName: userNameValidationResult
+      });
+
+  }, [profileData.name]);
+
+  // реагирует на изменение Email
+  useEffect(() => {
+    const { email } = profileData;
+
+      const userEmailValidationResult = Object.keys(validators.userEmail)
+      .map((errorKey) => {
+        const errorResult = validators.userEmail[errorKey](email);
+        return { [errorKey]: errorResult};
+      })
+      .reduce((acc, el) => ({ ...acc, ...el }), {});
+
+      setErrors({
+        ...errors,
+        userEmail: userEmailValidationResult,
+      });
+
+  }, [profileData.email]);
+
+  useEffect(() => {
+    setErrorTexts({
+      ...errorTexts,
+      userNameErrorText: errorMessages.userName[Object.keys(errors.userName).find((key) => {
+        return errors.userName[key] === true;
+      })]
+    });
+  }, [errors.userName]);
+
+  useEffect(() => {
+    setErrorTexts({
+      ...errorTexts,
+      userEmailErrorText: errorMessages.userEmail[Object.keys(errors.userEmail).find((key) => {
+        return errors.userEmail[key] === true;
+      })]
+    });
+  }, [errors.userEmail]);
+
   function handleChange (e) {
     setProfileData({
       ...profileData,
@@ -25,7 +112,6 @@ function Profile ({ onEdit, setEdit, reqError, reqErrorText, openMenu, handleEdi
 
   function setEditStatus () {
     onEdit ? setEdit(false) : setEdit(true);
-    console.log(onEdit);
   }
 
   function editProfile (e) {
@@ -38,10 +124,6 @@ function Profile ({ onEdit, setEdit, reqError, reqErrorText, openMenu, handleEdi
     handleSignOut();
   }
 
-    // временные переменные:
-    const validationMessage = "Здесь могла бы быть ваша ошибка";
-    const validationError = true;
-
   return(
     <>
     <Header bgColor="light" loggedIn={true} openMenu={openMenu} />
@@ -52,18 +134,18 @@ function Profile ({ onEdit, setEdit, reqError, reqErrorText, openMenu, handleEdi
           <div className="profile__form-field">
             <p className="profile__form-field-name">Имя</p>
             <input className="profile__form-input" type="text" name="name" minLength="2" maxLength="30" required disabled={!onEdit} value={onEdit ? profileData.name : userInfo.name} onChange={handleChange} />
-            <ErrorMessage errorText={validationMessage} type="profile-validation" />
+            <ErrorMessage errorText={errorTexts.userNameErrorText} type="profile-validation" />
           </div>
           <div className="profile__form-line"></div>
           <div className="profile__form-field">
             <p className="profile__form-field-name">E-mail</p>
             <input className="profile__form-input" type="email" name="email" minLength="2" maxLength="30" required disabled={!onEdit} value={onEdit ? profileData.email : userInfo.email} onChange={handleChange} />
-            <ErrorMessage errorText={validationMessage} type="profile-validation" />
+            <ErrorMessage errorText={errorTexts.userEmailErrorText} type="profile-validation" />
           </div>
         </div>
         {onEdit ? <div>
           {reqError && <ErrorMessage errorText={reqErrorText} type="request" />}
-          <input className="profile__save-button" type="submit" value="Сохранить" />
+          <input className="profile__save-button" type="submit" value="Сохранить" disabled={isSubmitDisabled} />
         </div>
         : <div>
         <input className="profile__button" type="button"  value="Редактировать" onClick={setEditStatus} />
